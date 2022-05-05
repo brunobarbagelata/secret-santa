550d4214bf95aee1464f91808e2eafe0489aa9b0
Bruno Barbagelata <bruno140685@gmail.com>
2022-05-05 17:25:20 -0400

    back again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,31 @@ function App() {
     <div>
       <h1>Hello!</h1>
       <h2>Working again</h2>
+      <form>
+        <label for="fname">First name:</label>
+        <br></br>
+        <input type="text" id="fname" name="fname"></input>
+        <br></br>
+        <label for="lname">Last name:</label>
+        <br></br>
+        <input type="text" id="lname" name="lname"></input>
+        <br></br>
+        <label for="Choose">Choose one:</label>
+        <br></br>
+        <input type="radio" id="html" name="fav_language" value="HTML"></input>
+        <label for="html">HTML</label>
+        <br></br>
+        <input type="radio" id="css" name="fav_language" value="CSS"></input>
+        <label for="css">CSS</label>
+        <br></br>
+        <input
+          type="radio"
+          id="javascript"
+          name="fav_language"
+          value="JavaScript"
+        ></input>
+        <label for="javascript">JavaScript</label>
+      </form>
     </div>
   );
 }
